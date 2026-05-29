b408aa08cf5c03e69f6b65ba2c1ccb09c70f6c93
Ayush Kumar <ash09aps@gmail.com>
2026-05-30 00:59:45 +0530

    feat: integrate socket.io for real-time communication and update dependencies

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
+const { log } = require('console');
 const app = require('./src/app')
-app.listen(3000,()=>{
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
+io.on("connection", (socket) => {
+    // ! IN-BUILT EVENT ONE 
+    console.log("User is connected");
+    // ! IN-BUILT EVENT TWO 
+    socket.on("disconnect",()=>{
+        console.log("User is disconnected");
+    })
+    // ! CUSTOM EVENT
+    socket.on("message",()=>{
+        console.log("Message event fired");
+    })
+    //  ! Another custom event
+     socket.on("chacha",(data)=>{
+        console.log(data);
+        
+        console.log("chacha event fired and hit chachi");
+    })
+});
+httpServer.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ const express = require('express')
 
 const app = express()
 
+app.get('/',(req,resp)=>{
+    resp.send("hello world!");
+})
  module.exports = app
