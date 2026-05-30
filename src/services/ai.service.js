537ffc6d098658455a150250409ee64650deed9d
Ayush Kumar <ash09aps@gmail.com>
2026-05-30 13:36:26 +0530

    feat: update AI response handling and refactor chat history management

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -2,10 +2,10 @@ const { GoogleGenAI, GenerateImagesResponse } = require("@google/genai");
 
 const ai = new GoogleGenAI({});
 
-async function generateResponse(prompt) {
+async function generateResponse(historyTimeline) {
     const response = await ai.models.generateContent({
         model : "gemini-2.5-flash",
-        contents: prompt
+        contents: historyTimeline
     })
     return response.text
 }
